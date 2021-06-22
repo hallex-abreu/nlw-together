9c1edf4e041507a681c1ec0f9a3e9ee1a0e8dd8f
max costa <maxcosta@maxcosta.me>
2021-06-22 09:18:07 -0300

    parcialmente concluido

--- src/screens/Signin/index.tsx
+++ src/screens/Signin/index.tsx
@@ -2,21 +2,23 @@ import React from 'react';
 import { 
   View, 
   Text, 
-  Image ,
-  StatusBar
+  Image 
 } from 'react-native';
-import { styles } from './styles';
+import { useNavigation } from '@react-navigation/native';
+
 import Illustration from '../../assets/illustration.png';
+import { styles } from './styles';
+
 import { ButtonIcon } from '../../components/ButtonIcon';
 
 export function Signin(){
+  const navigation = useNavigation();
+
+  function handleSignin(){
+    navigation.navigate('Home')
+  }
   return (
     <View style={styles.container}>
-      <StatusBar
-        barStyle="light-content"
-        backgroundColor="transparent"
-        translucent
-      />
       <Image 
         source={Illustration} 
         style={styles.image}
@@ -24,17 +26,17 @@ export function Signin(){
       />
       <View style={styles.content}>
         <Text style={styles.title}>
-          Organize {`\n`}
-          suas jogatinas {`\n`}
-          facilmente
+          Conecte-se {'\n'}
+          e organize suas {'\n'}
+          jogatinas
         </Text>
         <Text style={styles.subtitle}>
-          Crie grupos para jogar seus games {`\n`}
+          Crie grupos para jogar seus games {'\n'}
           favoritos com seus amigos
         </Text>
         <ButtonIcon 
           title="Entrar com Discord"
-          activeOpacity={0.7}
+          onPress={handleSignin}
         />
       </View>
     </View>
